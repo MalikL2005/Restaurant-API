d020c617ade4f1b70e8f1353dcd5a9ae837cfcab
MalikL2005 <malik.lorenz2005@gmail.com>
2024-09-01 18:02:55 +0200

    feat: Post method for adding bookings/menuitems.

--- little_lemon_restaurant/restaurant/views.py
+++ little_lemon_restaurant/restaurant/views.py
@@ -12,9 +12,21 @@ class bookingview(APIView):
         items = Booking.objects.all()
         serializer = bookingSerializer(items, many=True)
         return Response(serializer.data)
+    
+    def post(self, req):
+        serialized_new_booking = bookingSerializer(data=req.data)
+        if serialized_new_booking.is_valid():
+            serialized_new_booking.save()
+            return Response({'status': 'success', 'data': serialized_new_booking.data})
 
 class menuview(APIView):
     def get(self, req):
         menu_items = Menu.objects.all()
         menu_item_serializer = menuSerializer(menu_items, many=True)
         return Response(menu_item_serializer.data)
+    
+    def post(self, req):
+        serialized_new_menu_item = menuSerializer(data=req.data)
+        if serialized_new_menu_item.is_valid():
+            serialized_new_menu_item.save()
+            return Response({'status': 'success', 'data': serialized_new_menu_item.data})
