2d3eaaf9a4f739328170525b0b5289d2fe3dd7b1
MalikL2005 <malik.lorenz2005@gmail.com>
2024-09-02 18:54:17 +0200

    Setup for authentication

--- little_lemon_restaurant/restaurant/views.py
+++ little_lemon_restaurant/restaurant/views.py
@@ -4,6 +4,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Booking, Menu
 from .serializers import menuSerializer, bookingSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.mixins import PermissionRequiredMixin
 # from rest_framework.authentication import 
 
 # Create your views here.
@@ -25,3 +28,5 @@ class booking_two(ListCreateAPIView):
 class single_booking_two(RetrieveUpdateAPIView, DestroyAPIView):
     queryset = Booking.objects.all()
     serializer_class = bookingSerializer
+
+
