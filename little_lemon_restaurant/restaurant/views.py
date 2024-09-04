68387ed22630f24eb74a76505b8f51cee930424a
MalikL2005 <malik.lorenz2005@gmail.com>
2024-09-04 21:22:50 +0200

    feat: authentication + get user-token

--- little_lemon_restaurant/restaurant/views.py
+++ little_lemon_restaurant/restaurant/views.py
@@ -5,8 +5,7 @@ from rest_framework.response import Response
 from .models import Booking, Menu
 from .serializers import menuSerializer, bookingSerializer
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 # from rest_framework.authentication import 
 
 # Create your views here.
@@ -22,10 +21,12 @@ class singleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
     serializer_class = menuSerializer
 
 class booking_two(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = bookingSerializer
 
 class single_booking_two(RetrieveUpdateAPIView, DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = bookingSerializer
 
