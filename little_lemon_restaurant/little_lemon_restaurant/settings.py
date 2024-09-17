21282633173b965a3521e1f811959cc26fdc80d4
MalikL2005 <malik.lorenz2005@gmail.com>
2024-09-17 22:41:26 +0200

    fix

--- little_lemon_restaurant/little_lemon_restaurant/settings.py
+++ little_lemon_restaurant/little_lemon_restaurant/settings.py
@@ -22,9 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!3cc8$fv6e@h@4lay1n)5h)3dsnm%v=+bf1e%80xbwfbnv!)ij'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
